666416c96a26ac79e57d4b8d3fe02f94b54b6955
Davih Ferraz <davihlferraz@hotmail.com>
2023-05-08 22:22:26 -0300

    Adicionando TextField

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
+// ignore: unused_import
+// ignore_for_file: import_of_legacy_library_into_null_safe, duplicate_ignore
+
+// ignore: unused_import
+import 'dart:developer';
+
 import 'package:conversor_de_moedas/view/home/home_view.dart';
 import 'package:flutter/material.dart';
 // ignore: import_of_legacy_library_into_null_safe, unused_import
 import 'package:http/http.dart';
-import 'package:async/async.dart';
+import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?key=6b8fbd4d";
 
 void main() async {
-  http.Response response = await http.get(request);
-  print(
-    json.decode(response.body)["results"]["currencies"]["USD"],
-  );
+  // ignore: avoid_print
+  print(await getData());
   runApp(const MyApp());
 }
 
@@ -25,9 +29,19 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Conversor',
       theme: ThemeData(
+        inputDecorationTheme: const InputDecorationTheme(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.amber),
+          ),
+        ),
         primarySwatch: Colors.amber,
       ),
       home: const HomePage(),
     );
   }
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
 }
--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,50 +1,121 @@
+import 'package:conversor_de_moedas/main.dart';
 import 'package:flutter/material.dart';
 // ignore: import_of_legacy_library_into_null_safe, unused_import
 import 'package:http/http.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?key=6b8fbd4d";
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double? dolar;
+  double? euro;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        centerTitle: true,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Icon(Icons.attach_money),
-            Text('Conversor'),
-            Icon(Icons.attach_money),
-          ],
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 30),
-        child: Center(
-          child: Column(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: const [
-              Icon(
-                Icons.attach_money,
-                color: Colors.amber,
-                size: 100,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              //Reais
-              TextField(),
-              //Dólares
-              TextField(),
-              //Euros
-              TextField(),
+              Icon(Icons.attach_money),
+              Text('Conversor'),
+              Icon(Icons.attach_money),
             ],
           ),
         ),
-      ),
-    );
+        body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return const Center(
+                  child: Text(
+                    'Carregando Dados...',
+                    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              default:
+                if (snapshot.hasError) {
+                  return const Center(
+                    child: Text(
+                      'Erro ao Carregar Dados...',
+                      style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                      textAlign: TextAlign.center,
+                    ),
+                  );
+                } else {
+                  dolar = snapshot.data!["results"]["currencies"]["USD"]["buy"];
+                  euro = snapshot.data!["results"]["currencies"]["EUR"]["buy"];
+                  return SingleChildScrollView(
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.only(top: 30, right: 20, left: 20),
+                      child: Center(
+                        child: Column(
+                          children: const [
+                            Icon(
+                              Icons.monetization_on,
+                              color: Colors.amber,
+                              size: 100,
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            //Reais
+                            TextField(
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Reais",
+                                labelStyle: TextStyle(color: Colors.amber),
+                              ),
+                              style: TextStyle(color: Colors.amber),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+
+                            TextField(
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Dólares",
+                                labelStyle: TextStyle(color: Colors.amber),
+                              ),
+                              style: TextStyle(color: Colors.amber),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            TextField(
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Euros",
+                                labelStyle: TextStyle(color: Colors.amber),
+                              ),
+                              style: TextStyle(color: Colors.amber),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                }
+            }
+          },
+        ));
   }
 }
